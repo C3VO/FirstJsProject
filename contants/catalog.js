3464ea19ca467b89645de6c062cadddf4afd7b90
Vlas <pensioner555@gmail.com>
2023-08-08 06:12:20 +0300

    First JavaScript project

---
+++ contants/catalog.js
@@ -0,0 +1,50 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'PC',
+        img: 'https://i.ibb.co/C5LdWnN/U0503383.jpg',
+        price: 2199
+    },
+    {
+        id: 'el2',
+        name: 'PC parts',
+        img: 'https://www.pngmart.com/files/7/Graphics-Card-PNG-Transparent-Image.png',
+        price: 499
+    },
+    {
+        id: 'el3',
+        name: 'Monoblock',
+        img: 'https://i.ibb.co/mtVQyxZ/U0752009.jpg',
+        price: 4999
+    },
+    {
+        id: 'el4',
+        name: 'Notebook',
+        img: 'https://i.ibb.co/gj7HbjC/U0482057.jpg',
+        price: 3799
+    },
+    {
+        id: 'el5',
+        name: 'Smartphone',
+        img: 'https://yabloki.ua/media/cache/sylius_shop_changed_product_thumbnail/00/59/cae252303d9e66bcacdec0f1be3f.png',
+        price: 2199
+    },
+    {
+        id: 'el6',
+        name: 'Tablet',
+        img: 'https://images.macrumors.com/article-new/2013/09/ipad-mini-6-lineup.jpg',
+        price: 2799
+    },
+    {
+        id: 'el7',
+        name: 'Earphones',
+        img: 'https://www.energysistem.com/cdnassets/products/45173/rear_2000.jpg',
+        price: 499
+    },
+    {
+        id: 'el8',
+        name: 'Watch',
+        img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/FKN73?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1647901229415',
+        price: 299
+    },
+]
